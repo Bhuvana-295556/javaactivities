841a5bf65a05bf6981bbc15e76e51c984124f319
Bhuvana-295556 <82052062+Bhuvana-295556@users.noreply.github.com>
2021-06-05 15:28:48 +0530

    Update company.java

--- Activity 1/company.java
+++ Activity 1/company.java
@@ -1,1 +1,39 @@
+Public class company 
+{
+Private string name;
+Private string employes;
+Private string Teamlead;
+Public void getInput()
+{
+Scanner in = new Scanner (System.in);
+System.out.print(“Enter the name:”);
+name=in.next();
+System.out.print(“Enter the employes:”);
+address=in.next();
+System.out.print(“Enter the Teamlead:”);
+Mobile=in.next();
+}
+Public void display()
+{
+
+System.out.println(“Name: ” +name);
+System.out.println(“employes: ” +employes);
+System.out.println(“Teamlead : ” +Teamlead);
+}
+Public static void main(String[]args)
+{
+Employee e[] = new Employee[5];
+For(int i=0;i<5;i++)
+{
+e[i]=new employee();
+e[i].getInput();
+}
+For(int i=0;i<5;i++)
+{
+e[i].display();
+}
+}
+}
+
+
 
