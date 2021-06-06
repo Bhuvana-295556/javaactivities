c4998dd50b911133bc8f42450eb85c9d5c0b221b
Bhuvana-295556 <82052062+Bhuvana-295556@users.noreply.github.com>
2021-06-06 17:10:39 +0530

    Update abstractclass_shape.java

--- Activity 3/abstractclass_shape.java
+++ Activity 3/abstractclass_shape.java
@@ -1,39 +1,124 @@
+package test;
+
 import java.util.Scanner;
- 
-abstract class calcArea {
-    
-    abstract void findSquare(double s);
-    abstract void findCircle(double r);
+
+public class test{
+	
+   public static void main(String[] args){
+	   
+	   Scanner sc = new Scanner(System.in);
+			  
+	   System.out.println("Enter the Shape");
+	   
+	   String shape = sc.nextLine();
+	   
+	   int radius,length;
+	   
+
+	   if(shape == "Circle") {
+		   
+		   System.out.println("Enter the Radius");
+		   
+		   radius = sc.nextInt();
+		   
+		   Shape c = new Circle(radius);
+		   
+		   System.out.println("Circle Area is " + c.calculateArea());
+		   
+		   }
+	   
+	   else {
+		   
+		   System.out.println("Enter the dimensions");
+		
+		   length = sc.nextInt();
+		
+		   Shape s = new Square(length);
+
+		   System.out.println("Square Area is " + s.calculateArea());
+	   }
+	   
+
+	   
+  }
 }
- 
-class findArea extends calcArea {
- 
-    void findSquare(double s)
-    {
-        double area = s*s;
-        System.out.println("Area of Square: "+area);
-    }
-     
-    void findCircle(double r)
-    {
-        double area = 3.14*r*r;
-        System.out.println("Area of Circle: "+area);
-    }
+ package test;
+
+abstract public class Shape {
+	
+	   // Private member variable
+	   protected int value;
+	   
+	
+	   
+	  abstract public double calculateArea();
+	}
+
+ package test;
+
+public class Circle extends Shape{
+	
+	   // private instance variables
+	   private double radius;
+
+	
+	   public Circle(double radius) {
+	      this.radius = radius;
+	      System.out.println("Construced a Circle with Circle(radius)");  // for debugging
+	   }
+	  
+	   // public getters and setters for the private variables
+	   public double getRadius() {
+	      return this.radius;
+	   }
+	   
+	   public void setRadius(double radius) {
+	      this.radius = radius;
+	   }
+	  
+
+	 
+ public double calculateArea() {
+	
+	 return radius * radius * Math.PI;
+		
+	}
+
 }
-         
-class area {
-    public static void main(String args[])
-    {
-        double  r, s;
-        findArea area = new findArea();
-        Scanner get = new Scanner(System.in);
- 
-        System.out.print("\nEnter Side of a Square: ");
-        s = get.nextDouble();
-        area.findSquare(s);
- 
-        System.out.print("\nEnter Radius of Circle: ");
-        r = get.nextDouble();
-        area.findCircle(r);
-    }
-}   
+[9:05 PM, 6/5/2021] Yashaswini LT: Square.java
+[9:05 PM, 6/5/2021] Yashaswini LT: package test;
+
+public class Square extends Shape{
+	
+	private int l;
+	
+	public Square(int l) {
+		
+		this.l = l;
+		
+		
+	}
+	
+	//setter
+	
+	public void setDimensions(int l) {
+		
+		this.l = l;
+
+	  
+	}
+	  
+	//getter
+	
+	public int getLength() {
+		
+		return this.l;
+	}
+
+	public double calculateArea() {
+		
+		 return l*l;
+			
+		}
+
+}
